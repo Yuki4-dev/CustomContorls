a03eed71dfaa99418b0b83570068802fcd59dee6
LAPTOP-ITBNKE7K\sanak <cdg.akfg@gmail.com>
2021-10-31 17:31:23 +0900

    プロジェクト ファイルを追加します。

---
+++ CustomContorls/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomContorls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Page _CustomScrolbarPage = new CustomScrolbarPage();
+        private readonly Page _CustomComboBoxPage = new CustomComboBoxPage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrolBar_Selected(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Content = _CustomScrolbarPage;
+        }
+
+        private void ComboBox_Selected(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Content = _CustomComboBoxPage;
+        }
+    }
+}
